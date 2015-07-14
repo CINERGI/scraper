d9c5b1ee95c2984d517bec967285128fad2d486c
Aaron Gong <aagong@ucsd.edu>
2015-07-14 13:40:05 -0700

    updated files in metadataScraper

--- googleSearchScraper/googleSearchScraper/settings.py
+++ googleSearchScraper/googleSearchScraper/settings.py
@@ -9,10 +9,10 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'tutorial'
+BOT_NAME = 'googleSearchScraper'
 
-SPIDER_MODULES = ['tutorial.spiders']
-NEWSPIDER_MODULE = 'tutorial.spiders'
+SPIDER_MODULES = ['googleSearchScraper.spiders']
+NEWSPIDER_MODULE = 'googleSearchScraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
