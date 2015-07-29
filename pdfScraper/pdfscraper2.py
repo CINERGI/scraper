96110e67fadaa8d7e62ffc3649f5a1f58b1aab1a
edricx <edric.xiang@gmail.com>
2015-07-29 14:08:58 -0700

    Minor edits to pdfscraper2; cleaned up code

--- pdfScraper/pdfscraper2.py
+++ pdfScraper/pdfscraper2.py
@@ -1,9 +1,9 @@
 import urllib # will use scrapy instead later
 import json
 import os, glob
-import pdfminer
-import slate
-import time # temporary solution to prevent calling parsepdf() before download finishes
+# import pdfminer // Outdated and not working; instead call shell tool
+# import slate // Not working
+# import time  // No longer needed; urlretrieve pauses itself until dl finishes
 import subprocess
 
 def readJSON():
@@ -14,52 +14,45 @@ def readJSON():
                 if data[i][u'pdf']:
                     title = str(data[i][u'title'])[3:-2]
                     url = str(data[i][u'pdf'])
-                    # xmlid = str(data[i][u'id'])
-                print "\n" + str(i + 1) + ") Title: " + title
+                    # xmlid = str(data[i][u'id']) // may implement later
+                print "\n" + str(i + 1) + ") TITLE: " + title
                 download(title, url) #, xmlid)
             except KeyError:
                 pass
     print "File should be closed now, waiting to finish downloads...\n"
-    # time.sleep(20)
+    # time.sleep(20) // No longer needed; urlretrieve waits until dl finishes
     # print "Slept 20 secs, calling parsepdf()...\n"
     parsepdf()
 
 
 def download(title, url): #, xmlid):
     file = urllib.urlretrieve(url, "./PDFs/" + title + ".pdf")
-    print "Downloading: " + title + ".pdf\n"
+    print "DOWNLOADED: " + title + ".pdf\n"
 
 def parsepdf():
     print "parsepdf() was called...\n"
     os.chdir("./PDFs")
     for pdf in glob.glob("*.pdf"):
-        pdftitle = pdf[:-4] + ".txt"
+        filename = pdf[:-4] + ".txt"
 
-        print "Reading:" + pdf
+        print "READING: " + pdf
         proc = subprocess.Popen(['pdf2txt.py', '-m 8', pdf], stdout=subprocess.PIPE)
         output = proc.stdout.read()
 
-        print "Writing: " + pdftitle + "\n"
+        print "WRITING: " + filename + "\n"
 
         savepath = "/home/edric/scraper/pdfScraper/Text/"
-        filepath = os.path.join(savepath, pdftitle)
-        with open(filepath, 'w') as textfile:
-            textfile.write(output)
+        filepath = os.path.join(savepath, filename)
+        with open(filepath, 'w') as f:
+            f.write(output)
 
-    print "\n                     ============[PROGRAM FINISHED]============\n"
+    print "\n                  ============[PARSING FINISHED]============\n"
 
-
-
-
-
-
-
-# Calling pdf2txt.py script from pdfminer; only thing that works // scratch that
-
-
+# TO DO: extract abstract/intro from txt. Problem is variations in formatting.
+# See ./Text/ folder for examples of quirks of the pdf scraper
+# Issues: random(?) newlines, Unicode form feed characters
 
 def main():
     readJSON()
-    # parsepdf()
 
 main()
